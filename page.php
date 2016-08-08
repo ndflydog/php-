9474ce0b4781fe11fa22a544974209d82189e22f
jinxue <hejinxue001@163.com>
2016-08-08 22:48:49 +0800

    page

--- page.php
+++ page.php
@@ -5,6 +5,8 @@
  */
 class Page
 {
+    protected $count;
+
     protected $pageSize;
 
     protected $offset;
@@ -13,23 +15,30 @@ class Page
 
     protected $current;
 
-    protected $perPage;
+    protected $prevPage;
 
     protected $nextPage;
 
+    protected $url;
+
     #初始化分页信息
-    public function __construct($count = null, $pageSize = 30, $current = 1)
+    public function __construct($count = null, $pageSize = 30, $current = 1, $url = null)
     {
         if(!$count) {
-            throw new \Exception ('分页总数未设置');
+            throw new \Exception ('pages count not set');
         }
         $this->count = $count;
         $this->pageSize = $pageSize;
         $this->limit = ($current - 1) * $pageSize;
         $this->offset = $pageSize;
         $this->current = $current;
-        $this->perPage = $current - 1;
-        $this->nextPage = $current + 1;
+        $this->prevPage = ($current - 1 == 0) ? 1 : ($current - 1) ;
+        $this->nextPage = ($current + 1 > $this->getPages()) ? $this->getPages : ($current + 1);
+        if(!$url) {
+            #默认是当前路径
+        }else {
+            $this->url = $url;
+        }
     }
 
     #获取分页信息
@@ -38,15 +47,17 @@ class Page
         if($this->$name) {
             return $this->$name;
         }else {
-            throw new \Exception("$name 属性不存在!");
+            throw new \Exception("$name property not exists");
         }       
     }
 
     #设置分页信息
     public function __set($name, $value)
     {
-        if($this->$name) {
+        if(property_exists($this, $name)) {
             $this->$name = $value;
+        }else {
+            throw new \Exception("$name property not be set");
         }
     }
 
@@ -55,6 +66,53 @@ class Page
     {
         return ceil($this->count/$this->pageSize);
     }
+
+    #生成分页链接
+    public function generatePagination()
+    {
+        $length = $this->getPages();
+        $page = '';
+        for($i = 1; $i <= $length; $i++) {
+            $active = ($this->current == $i) ? 'active' : '';    
+            $url = "$this->url?current=$i&pageSize=$this->pageSize";
+            $page .= <<<EOT
+<li><a href="$url" class="$active">$i</a></li>
+EOT;
+        }
+        $prevPage = '<li><a href="'.$this->url.'?current='.$this->prevPage.'">上一页</a></li>';
+        $nextPage = '<li><a href="'.$this->url.'?current='.$this->nextPage.'">下一页</a></li>';
+        $page = $prevPage.$nextPage.'<ul class="pagination">'.$page.'</ul>';
+        return $page;
+    }
+
+    #每页显示
+    public function generatePerPage()
+    {
+        $select = <<<EOT
+<select onchange="window.location=this.value>
+    <option value="{$this->url}?current={$this->current}&pageSize=20">20</option>
+    <option value="{$this->url}?current={$this->current}&pageSize=50">50</option>
+    <option value="{$this->url}?current={$this->current}&pageSize=100">100</option>
+</select>
+EOT;
+        return $select;
+    }
+
+    #跳页
+    public function generatePageSkit()
+    {
+        $length = $this->getPages();
+        $skit = '';
+        for($i = 1; $i <= $length; $i++) {
+            $skit .= <<<EOT
+<option value="{$this->url}?current=$i&pageSize={$this->pageSize}">$i</option>
+EOT;
+        }
+        return '<select onchange="window.location=this.value">'.$skit.'</select>';
+    }
 }
 
-(new Page(10))->pageSize;
+$page = new Page(100);
+$page->pageSize = 30;
+$page->url = 'index';
+echo $page->generatePageSkit();
