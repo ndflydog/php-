26f038c0acfa053cf11c1be78a773ba7788c50b8
fly <fly@localhost.localdomain>
2016-08-10 11:52:13 +0800

    事件相应

--- page.js
+++ page.js
@@ -9,8 +9,14 @@ var page = {
     "pagination" :   "#pagination",
     "perpage"   :   "#perpage",
     "tbody"     :   "#tbody",
+    "form"      :   "form",
+    //向服务器传递的信息对象
+    "search"    :   {
+        "pageSize" : this.pageSize,
+        "current"  : this.current,    
+    },
     //获取分页信息
-    "getResult" :   function (type = 'get', data = {pageSize:this.pageSize, current:this.current}, dataType = 'json') {
+    "getResult" :   function (type = 'get', data = this.search, dataType = 'json') {
         var that = this;
         $.ajax({
             url         :   this.url,
@@ -22,7 +28,7 @@ var page = {
             success     :   this.success,
         });
     },
-    //定义格式为 {code = 0,1 msg = '成功?失败?' data = [{'total' : 1}, {'result' : array}]}    
+    //定义格式为 {code = 0,1 msg = '成功?失败?' data = [{'total' : 1}, {'result' : array}, {field:[]}]}    
     "success"   :   function(data) {
         console.log(data);
         if(data.code == 0) {
@@ -49,8 +55,10 @@ var page = {
         var prevPage = (this.current - 1 > 0) ? (this.current - 1) : 1;
         var nextPage = (this.current + 1 < this.pages) ? (this.current + 1) : this.pages;
         prevPage = '<li class = "'+(prevPage == 1 ? "disabled" : "")+'"><a data-value = "'+prevPage+'" data-url="'+this.url+'?current='+i+'&pageSize='+prevPage+'&pageSize='+this.pageSize+'"><<</a><li>';
-        nextPage = '<li class = "'+(nextPage == this.pages ? "disabled" : "")+'"><a data-value = "'+nextPage+'" data-url="'+this.url+'?current='+i+'&pageSize='+nextPage+'&pageSize='+this.pageSize+'">>></a><li>'; 
-        return '<ul class="pagination">'+prevPage+pagination+nextPage+'</ul>';
+        nextPage = '<li class = "'+(nextPage == this.pages ? "disabled" : "")+'"><a data-value = "'+nextPage+'" data-url="'+this.url+'?current='+i+'&pageSize='+nextPage+'&pageSize='+this.pageSize+'">>></a><li>';
+        lastPage = '<li class = ""><a data-value = "'+this.pages+'" data-url="">>>></a><li>';
+        firstPage = '<li class = ""><a data-value = "'+1+'" data-url=""><<<</a><li>';
+        return '<ul class="pagination">'+firstPage+prevPage+pagination+nextPage+lastPage+'</ul>';
      },
      //每页显示多少#perPage
      "generatePerPage" : function () {
@@ -75,9 +83,9 @@ var page = {
          return table;
      },
      //select事件函数设置此对象的pageSize
-     "perPageClick" : function (select = "#perpage") {
+     "perPageClick" : function () {
          var that = this;
-         $(select).on('change', "select", function (event){
+         $(this.perpage).on('change', "select", function (event){
              event.preventDefault();
              var pageSize = $(event.target).val();   
              page.pageSize = pageSize;
@@ -87,10 +95,32 @@ var page = {
      //重绘数据.paginaion
      "paginationClick" : function () {
          var that = this;
-         $('#pagination').on('click', 'a', function (event) {
+         $(this.pagination).on('click', 'a', function (event) {
              event.preventDefault();
              var value = $(event.target).data('value');
              page.current = value;
+             page.getResult();
+         });
+     },
+     //搜索 生成搜索的{a:b}
+     "searchfunc" : function () {
+         return {
+             pageSize:this.pageSize, 
+             current:this.current   
+         }
+     },
+     //搜索设置事件
+     "searchResponse" : function () {
+         $(this.form).on('keydown', 'input', function(event) {
+             event.preventDefault();
+             var code = event.code;
+             if(code != 13) {
+                 return false;
+             }
+             //这个根据需要要重写
+             var value = $(event.target).val();
+             var name = $(event.target).attr('name');
+             page.search.name = value;
              page.getResult();
          });
      }
