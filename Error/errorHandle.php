2dbf9a4e203bb9932af1ea0019ff749f1b554d1b
mrHe <hejixnue001@163.com>
2016-12-07 15:27:19 +0800

    php中错误异常处理函数

--- Error/errorHandle.php
+++ Error/errorHandle.php
@@ -4,10 +4,16 @@
 直接调用set_error_handler()中指定的回调函数。并将错误相关的信息，比如说错误行号，文件
 名等信息传递到该回调函数。设置set_error_handler()函数以后，原先使用的@这个屏蔽错误的
 语法糖就失去了作用
+set_error_handler()函数并不会接管所有的错误,Fatal_error, Parse_error等错误类型该
+函数是接管不了的。Parse_error是解析错误，是php解释器解释阶段发生的错误，我们是不可能接管
+这个错误的。对于致命错误，我们可以通过register_shutdown_function()函数来进行处理。需要
+注意的是，这个函数并不能提我们接管错误信息，而是在程序彻底"死掉"之前，执行一下器指定的回调
+函数。有点类似与php类中的析构函数。
 */
 function error_handler($errno, $errstr, $errfile, $errlime)
 {
-    echo '错误捕获';  
+    echo '错误捕获';
+    #写入日志  
     switch($errno) {
         case 2:
             $error_type = "E_WARNING";
@@ -29,7 +35,7 @@ function error_handler($errno, $errstr, $errfile, $errlime)
 }
 set_error_handler("error_handler");
 try {
-    fopen('/tmp/ttttt.log', 'r');    #php老旧函数直接抛出错误 不能用try catch 异常捕获
+    @fopen('/tmp/ttttt.log', 'r');    #php老旧函数直接抛出错误 不能用try catch 异常捕获
 } catch (\Exception $e) {
     echo '异常捕获';
     echo $e->getMessage();
