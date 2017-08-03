c2e519b9fb3c4e2a48ad29602014bbb6705366b7
jinxue <hejinxue001@163.com>
2017-08-03 20:34:18 +0800

    队列通信php

--- PDO/pdo.php
+++ PDO/pdo.php
@@ -7,13 +7,30 @@
 
 $dsn = "mysql:host=127.0.0.1;dbname=test";
 echo '数据插入'.PHP_EOL;
+$start = time();
 try {
     $pdo = new \PDO($dsn, 'root', '');
-    for ($i = 0; $i < 100; $i++) {
+    $value = '';
+    $c = 0;
+    for ($i = 0; $i < 1000000; $i++) {
         $tmp = 'haha'.$i;
-        $pdo->exec("insert into test (name) values ('".$tmp."');");
+        $c++;
+        echo $tmp.PHP_EOL;
+        $value .= strtr('(":tmp"),', [':tmp' => $tmp]);
+        if ($c == 50000) {
+            echo '插入'.PHP_EOL;
+            $value = substr($value, 0, -1);    
+            $pdo->exec("insert into user (test) values ".$value);
+            $value = '';
+            $c = 0;                          
+        }
     }
-    echo 1;
+    if ($c > 0) {
+        $value = substr($value, 0, -1);
+        $pdo->exec("insert into user (test) values ".$value);  
+    }  
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
+
+echo time() - $start;
--- process/ProcessQueueClass.php
+++ process/ProcessQueueClass.php
@@ -8,12 +8,18 @@ class ProcessQueueClass
 
     protected $cpu_cores;
 
+    protected $callback;
+
+    protected $msgQueue;
+
     public function __construct($cores = 0)
     {
         $this->cpu_cores = $cores ? $cores : substr(trim(exec('cat /proc/cpuinfo| grep "cpu cores"| uniq')), -1);
+        $id = ftok(__FILE__,'m');
+        $this->msgQueue = msg_get_queue($id);
     }
 
-    protected function createProgress($callback, array $param)
+    protected function createProgress()
     {
         $pid = pcntl_fork();
         if ( $pid == -1) {
@@ -22,7 +28,7 @@ class ProcessQueueClass
         } else if ($pid == 0) {
             // 子进程执行程序
             $pid = posix_getpid();
-            call_user_func_array($callback, $param);
+            $this->work();
             exit("({$pid})child progress end!\n");
         }else{
             // 父进程执行程序
@@ -30,14 +36,49 @@ class ProcessQueueClass
         }
     }
 
-    public static function done($cores = 0, $callback, array $param)
+    public function done()
     {
-        while(!empty($childList)){
+        for ($i = 0; $i < $this->cpu_cores; $i++) {
+            $pid = $this->createProgress();
+            $this->child_arr[$pid] = 1;
+        }
+        while(!empty($this->child_arr)){
             $childPid = pcntl_wait($status);
             if ($childPid > 0){
-                unset($childList[$childPid]);
+                unset($this->child_arr[$childPid]);
             }
         }
+
+        return $this->finish();
     }
 
+    #覆盖此方法
+    public function work()
+    {
+        $arr = [1,2,3,4,5,6,7,8,9];
+        $pid = posix_getpid();
+        $sum = array_sum($arr);
+        echo "$pid 进程计算的值为$sum".PHP_EOL;
+        msg_send($this->msgQueue, 1, $sum);
+    }
+
+    #结束方法
+    public function finish()
+    {
+        $sum = 0;
+        while (1) {
+            $msgType = '';
+            msg_receive($this->msgQueue, 1, $msgType, 1024,$message, true, MSG_IPC_NOWAIT);
+            if (!$message) break;
+            $sum += $message;
+        }
+        return $sum;
+    }
+
+    public function __destroy()
+    {
+        msg_remove_queue($this->msgQueue);
+    }
 }
+
+echo (new ProcessQueueClass())->done();
